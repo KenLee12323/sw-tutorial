8a3ad2339d74bae6933d9ab7a86eadba43b5c17e
KenLee12323 <lolqwer12323@gmail.com>
2019-09-22 23:49:52 +0800

    Finished assignment1
    How my clang got such error: 'stdio.h file not found.
    
    I didn't compile it..

--- assignment1/implementation.c
+++ assignment1/implementation.c
@@ -1,9 +1,35 @@
-//ITSC:
+//ITSC: 20342478
 #include "implementation.h"
 
 Result getLargestConsecutiveChar(char *str)
 {
     Result r;
     // implementation here
+    // if empty string return '\0' and 1
+    if(strlen(str)==0){
+    	r.c='\0';
+    	r.length=1;
+    	return r;
+    }
+
+    unsigned short len = strlen(str);
+    unsigned short count = 0;
+    char solution = str[0];
+    unsigned short currentCount = 1;
+    //O(1) time
+    for(unsigned short i=0;i<len;i++){
+        // if next char is same with current character
+        if(i<len-1 && str[i]==str[i+1]){
+            currentCount+=1;
+        }else{
+            if(currentCount>count){
+                solution = str[i];
+                count = currentCount;
+            }
+            currentCount = 1;
+        }
+    }
+    r.c = solution;
+    r.length = count;
     return r;
 }
