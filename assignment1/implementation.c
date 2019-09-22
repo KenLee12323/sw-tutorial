caf74bb0319739dd4aa88b431750aadb67cd4f72
Who Sucking Care <lolqwer12323@gmail.com>
2019-09-23 01:33:02 +0800

    Finally Fixed -- fatal error: 'stdio.h' not found

--- assignment1/implementation.c
+++ assignment1/implementation.c
@@ -1,9 +1,38 @@
-//ITSC:
+//ITSC: 20342478
+//!!! for solving 'stdio.h' file not found error
+//clang implementation.c tests.c --target=i686-pc-mingw32
+
 #include "implementation.h"
 
 Result getLargestConsecutiveChar(char *str)
 {
     Result r;
     // implementation here
+    // if empty string return '\0' and 1
+    if(strlen(str)==0){
+    	r.c='\0';
+    	r.length=1;
+    	return r;
+    }
+
+    unsigned short len = strlen(str);
+    unsigned short count = 0;
+    char solution = str[0];
+    unsigned short currentCount = 1;
+    //O(1) time
+    for(unsigned short i=0;i<len;i++){
+        // if next char is same with current character
+        if(i<len-1 && str[i]==str[i+1]){
+            currentCount+=1;
+        }else{
+            if(currentCount>count){
+                solution = str[i];
+                count = currentCount;
+            }
+            currentCount = 1;
+        }
+    }
+    r.c = solution;
+    r.length = count;
     return r;
 }
